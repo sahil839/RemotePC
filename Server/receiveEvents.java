388ba38dcd2bd67d08768018293ace5f64b99418
sahil839 <sahilbatra839@gmail.com>
2021-01-30 00:37:36 +0530

    server: Add new event type "CLOSE_CONNECTION" which will be sent when back button is pressed, to close the socket and for the same purpose separate variable is used in the loop.

--- Server/receiveEvents.java
+++ Server/receiveEvents.java
@@ -4,18 +4,24 @@ import java.io.*;
 class receiveEvents extends Thread{
 	String event_key;
 	Socket connected_socket;
+	private volatile Boolean receive_events;
 	receiveEvents(Socket sc) {
 		connected_socket = sc;
+		receive_events = true;
 		start();
 	}
 	public void run() {
 		try {
 			ObjectInputStream ip_stream = new ObjectInputStream(connected_socket.getInputStream());
-			while(true) {
+			while(receive_events) {
 				event_key = (String)ip_stream.readObject();
 				switch (event_key) {
 					case "SEND_SCREEN":
 						new sendCurrentScreen(connected_socket);
+						break;
+					case "CLOSE_CONNECTION":
+						receive_events = false;
+						connected_socket.close();
 						break;
 				}
 
