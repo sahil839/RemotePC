bb229405a52825d57cdd3cb91c752c9bf320c9c8
sahil839 <sahilbatra839@gmail.com>
2021-02-14 15:16:52 +0530

    client: Modify file share UI/UX to disable buttons while file is being transferred and show the status and file name correctly.

--- Client/app/src/main/java/com/example/client/SendFile.java
+++ Client/app/src/main/java/com/example/client/SendFile.java
@@ -1,10 +1,15 @@
 package com.example.client;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.InputStream;
 import java.io.ObjectOutputStream;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SendFile extends Thread{
     ObjectOutputStream opStream;
@@ -12,11 +17,18 @@ public class SendFile extends Thread{
     Context context;
     String fileName;
     Long fileSize;
-    SendFile(Uri uri, Context c, String name, Long size) {
+    Activity fileActivity;
+    Button selectFile, sendFile;
+    TextView filePathTxt;
+    SendFile(Uri uri, Context c, String name, Long size, Activity activity, Button selectbtn, Button sendbtn, TextView pathtxt) {
         file_uri = uri;
         context = c;
         fileName = name;
         fileSize = size;
+        fileActivity = activity;
+        selectFile = selectbtn;
+        sendFile = sendbtn;
+        filePathTxt = pathtxt;
         start();
     }
     public void run() {
@@ -37,6 +49,9 @@ public class SendFile extends Thread{
                 opStream.flush();
             }
             opStream.flush();
+            selectFile.postDelayed(()-> selectFile.setEnabled(true), 500);
+            sendFile.postDelayed(() -> sendFile.setEnabled(true), 500);
+            filePathTxt.setText("");
         } catch (Exception e){
             e.printStackTrace();
         }
